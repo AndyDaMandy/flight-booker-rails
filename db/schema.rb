a9349805044c15139e963a5bcdc7206dbb81fe79
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-08-27 12:48:42 -0400

    seeded a few db items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_27_000941) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_27_162557) do
+  create_table "airports", force: :cascade do |t|
+    t.string "airport_code"
+    t.string "airport_name"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+airports = Airport.create([
+    {   
+        airport_name: "Hartsfield-Jackson Atlanta International Airport",
+        airport_code: "ATL", 
+        city: "Atlanta", 
+        state: "GA", 
+        country: "USA" 
+    },
+      {
+        airport_name: "Dallas/Fort Worth International Airport",
+        airport_code: "DFW",
+        city: "Dallas & Ft. Worth",
+        state: "TX",
+        country: "USA"
+      },
+      {
+        airport_name: "Denver International Airport",
+        airport_code: "DEN",
+        city: "Denver",
+        state: "CO",
+        country: "USA"
+      },
+      {
+        airport_name: "Tokyo International Airport",
+        airport_code: "HND",
+        city: "Tokyo",
+        state: "Tokyo",
+        country: "Japan"
+      }
+      ])
