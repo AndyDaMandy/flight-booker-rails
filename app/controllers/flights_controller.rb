517c78d7c5853d8a79f92308b565825b9235142e
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-08-31 08:59:29 -0400

    building search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,19 @@
 class FlightsController < ApplicationController
     def index
         @flights = Flight.all
+        @arrival_options = Flight.all.map{ |f| [ f.arrival_airport.airport_name ]}
+    end
+
+    def search
+        if params[:search].blank?
+            redirect_to flights_path and return
+        else
+            @parameter = params[:search].downcase
+            @results = Flight.all.where("lower(name) LIKE :search", search: "%#{parameter}%")
+
+        end
+    end
+    def show
+        @flight = flight.find(params[:id])
     end
 end
