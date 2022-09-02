0b095c45031098ee280fa34a5632dd870116de3c
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-09-02 15:51:24 -0400

    fixing search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,18 +5,22 @@ class FlightsController < ApplicationController
         @departure_options = Flight.all.map{ |f| [ f.departing_airport.airport_name ]}
         @departure_date = Flight.all.map{ |f| [ f.departure_date ] }
         @arrival_date = Flight.all.map{ |f| [ f.arrival_date ] }
+        if request.get?
+        @parameter = params[:search]
+        @results = Flight.all.where("lower(name) LIKE :search", search: "%#{@parameter}%")
+        end
     end
 
     def search
         if params[:search].blank?
             redirect_to flights_path and return
         else
+            redirect_to flights_search_path
             @parameter = params[:search].downcase
             @results = Flight.all.where("lower(name) LIKE :search", search: "%#{parameter}%")
-
         end
     end
     def show
-        @flight = flight.find(params[:id])
+        @flight = Flight.find(params[:id])
     end
 end
