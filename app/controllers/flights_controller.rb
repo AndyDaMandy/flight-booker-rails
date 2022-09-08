fc2f234f414ea251c2a03eb68031b1f3dbafca50
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-09-08 10:05:02 -0400

    adjusted scopes

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,7 +5,9 @@ class FlightsController < ApplicationController
         @departure_options = Flight.all.map{ |f| [ f.departing_airport.airport_name ]}
         @departure_date = Flight.all.map{ |f| [ f.departure_date ] }
         @arrival_date = Flight.all.map{ |f| [ f.arrival_date ] }
-
+        if params[:flight]
+            @searched_fights = Flight.where(flight)params
+        end
         @flights = @flights.by_arrival_airport(params[:arrival_airport]) if params[:arrival_airport].present?
     end
 
@@ -21,7 +23,12 @@ class FlightsController < ApplicationController
         @flight = Flight.find(params[:id])
     end
 
+    private
+
     def flight_params
         params.require(:flight).permit(:arrival_airport_id, :departing_airport_id, :departure_date, :arrival_date)
+    end
+    def search_params
+        params.permit(:arrival_airport_id, :departing_airport_id, :departure_date, :arrival_date)
     end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,10 +2,10 @@ class Flight < ApplicationRecord
     belongs_to :departing_airport, class_name: "Airport"
     belongs_to :arrival_airport, class_name: "Airport"
 
-    scope :by_departure_date, -> { |departure_date| where(departure_date: departure_date) }
-    scope :by_arrival_date, -> { |arrival_date| where(arrival_date: arrival_date) }
-    scope :by_departure_airport, -> {|departure_airport| where(departure_airport: departure_airport) }
-    scope :by_arrival_airport, -> {|arrival_airport| where(arrival_airport: arrival_airport)}
+    scope :by_departure_date, -> (start_date, end_date) { where(departure_date: start_date..end_date) }
+    scope :by_arrival_date, -> (start_date, end_date) { where(arrival_date: start_date..end_date) }
+    scope :by_departure_airport, ->  (departure_airport) { where(departing_airport_id: departure_airport) }
+    scope :by_arrival_airport, -> (arrival_airport) { where(arrival_airport_id: arrival_airport)}
     def self.search(search)
         if search
             flight = Flight.find_by(name: search)
