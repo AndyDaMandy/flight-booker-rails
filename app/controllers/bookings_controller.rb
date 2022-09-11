9ba24da898e307b18210ad73f3c618f6441a9dfa
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-09-11 14:35:17 -0400

    form goes to booking now

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,10 @@
 class BookingsController < ApplicationController
+    def show
+
+    end
+    def new
+        @booking = Booking.new
+        @flight = Flight.find(params[:flight])
+        @passenger = params[:passenger]
+    end
 end
