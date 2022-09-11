defe1718c09c54a81724e1ec9b6c0ccf81be1214
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-09-10 21:36:49 -0400

    set up filter and it works!

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,15 +2,15 @@ class FlightsController < ApplicationController
     def index
         @flights = Flight.all
 
-        @flights = @flights.by_departing_airport(params[:departing_airport]) if params[:departing_airport].present?
+        @flights = @flights.by_departing_airport(params[:departing_airport])if params[:departing_airport].present?
         @flights = @flights.by_arrival_airport(params[:arrival_airport]) if params[:arrival_airport].present?
         @flights = @flights.by_departure_date(params[:departure_date]) if params[:departure_date].present?
         @flights = @flights.by_arrival_date(params[:arrival_date]) if params[:arrival_date].present?
         #@flights = Flight.search(params[:search])
-        @arrival_options = Flight.all.map{ |f| [ f.arrival_airport.id ]}
-        @departure_options = Flight.all.map{ |f| [ f.departing_airport.id ]}
-        @departure_date = Flight.all.map{ |f| [ f.departure_date ] }
-        @arrival_date = Flight.all.map{ |f| [ f.arrival_date ] }
+        @arrival_options = Flight.all.map{ |f| [ f.arrival_airport.airport_name, f.arrival_airport.id ]}.uniq
+        @departure_options = Flight.all.map{ |f| [ f.departing_airport.airport_name, f.departing_airport.id ]}.uniq
+        @departure_date = Flight.all.map{ |f| [ f.departure_date ] }.uniq
+        @arrival_date = Flight.all.map{ |f| [ f.arrival_date ] }.uniq
 =begin        if params[:flight]
             @searched_fights = Flight.where(:flight)params
         end
